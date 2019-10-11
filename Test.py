4267b2089171084c4e04e7fbdedc79d48d8d48fc
Himayeth Ali <himayeth@gmail.com>
2019-10-11 20:26:10 +0300

    email validation

--- Test.py
+++ Test.py
@@ -1,4 +1,17 @@
-def printer():
-    print("Function call to print this message")
+import re
 
-print("msg 1")
+regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+def check(email):
+    if(re.search(regex,email)):
+        print("Valid Email")
+
+    else:
+        print ("Invalid email")
+
+
+if __name__ == '__main__':
+    email ="ankitrai@gmail.com"
+    check(email)
+
+    email = "ankitrai"
+    check(email)
